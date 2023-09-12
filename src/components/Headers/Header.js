6f43c7a020f4640a38f798b81e92354a6d2b9921
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2023-09-12 16:14:52 +0600

    field added

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -1,33 +1,14 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.3
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
-// reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 
 const Header = () => {
   return (
     <>
-      <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+      <div className="header bg-gradient-info pb-5 pt-5 pt-md-8">
         <Container fluid>
           <div className="header-body">
             {/* Card stats */}
             <Row>
-              <Col lg="6" xl="3">
+              {/* <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -56,8 +37,8 @@ const Header = () => {
                     </p>
                   </CardBody>
                 </Card>
-              </Col>
-              <Col lg="6" xl="3">
+              </Col> */}
+              {/* <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -84,8 +65,8 @@ const Header = () => {
                     </p>
                   </CardBody>
                 </Card>
-              </Col>
-              <Col lg="6" xl="3">
+              </Col> */}
+              {/* <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -112,8 +93,8 @@ const Header = () => {
                     </p>
                   </CardBody>
                 </Card>
-              </Col>
-              <Col lg="6" xl="3">
+              </Col> */}
+              {/* <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -140,7 +121,7 @@ const Header = () => {
                     </p>
                   </CardBody>
                 </Card>
-              </Col>
+              </Col> */}
             </Row>
           </div>
         </Container>
--- src/views/pages/Blog/BlogCreateEdit.js
+++ src/views/pages/Blog/BlogCreateEdit.js
@@ -1,38 +1,63 @@
 // reactstrap components
 import {
-  Badge,
   Card,
   CardHeader,
-  CardFooter,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-  Table,
   Container,
   Row,
   Button,
   CardBody,
+  FormGroup,
+  Col,
 } from "reactstrap";
-import axios from "axios"; // core components
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Select from "react-select";
+
 import Header from "components/Headers/Header.js";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { GET_BLOG_API } from "core/apiEndpoints";
+import { getPublicData } from "core/apiClient";
+import { useParams } from "react-router-dom";
 
 const BlogCreateEdit = () => {
   const [data, setData] = useState({});
-  // http://18.141.173.147:5008/post/blogs?page=1&limit=10
-  async function getPosts() {
-    const resp = await axios.get(
-      "http://18.141.173.147:5008/post/blogs?page=1&limit=10"
-    );
-    setData(resp.data);
+  const { id } = useParams();
+
+  async function getPost(ID) {
+    await getPublicData(GET_BLOG_API + ID)
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
-    getPosts();
+    getPost(id);
   }, []);
 
   console.log(data);
+
+  const initialValues = {
+    title: "",
+    tags: [],
+    category: "",
+    content: "",
+  };
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    // tags: Yup.string().required("Tags is required"),
+    category: Yup.string().required("Category is required"),
+    content: Yup.string().required("Content is required"),
+  });
+
+  const handleSubmitMethod = (values) => {
+    // Handle form submission here
+    console.log(values);
+  };
+
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
   return (
     <>
@@ -44,10 +69,234 @@ const BlogCreateEdit = () => {
           <div className="col">
             <Card className="shadow">
               <CardHeader className="border-0">
-                <h3 className="mb-0">Card tables</h3>
+                <h3 className="mb-0">BLOG {id ? "EDIT" : "CREATE"}</h3>
               </CardHeader>
               <CardBody>
-                <h1>This is Create and edit</h1>
+                <Col className="order-xl-1" xl="8">
+                  <Card className="bg-secondary shadow">
+                    <CardBody>
+                      <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={handleSubmitMethod}
+                      >
+                        {(formikProps) => (
+                          <Form onSubmit={formikProps.handleSubmit}>
+                            <h6 className="heading-small text-muted mb-4">
+                              Insert You Blog
+                            </h6>
+                            <div className="pl-lg-4">
+                              <Row>
+                                <Col md="12">
+                                  <FormGroup>
+                                    <label
+                                      className="form-control-label"
+                                      htmlFor="input-address"
+                                    >
+                                      Title
+                                    </label>
+                                    <Field
+                                      className="form-control form-control-alternative"
+                                      // defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                                      id="input-address"
+                                      name="title"
+                                      placeholder="Blog Title"
+                                      type="text"
+                                    />
+                                    <ErrorMessage
+                                      name="title"
+                                      component="div"
+                                      className="text-danger"
+                                    />
+                                  </FormGroup>
+                                </Col>
+
+                                <Col lg="6">
+                                  <FormGroup>
+                                    <label
+                                      className="form-control-label"
+                                      htmlFor="input-title"
+                                    >
+                                      Tags
+                                    </label>
+                                    <Select
+                                      className="form-control-alternative"
+                                      type="text"
+                                      name="tags"
+                                      onChange={(option) => {
+                                        option
+                                          ? formikProps.setFieldValue(
+                                              "tags",
+                                              option
+                                            )
+                                          : formikProps.setFieldValue(
+                                              "tags",
+                                              ""
+                                            );
+                                      }}
+                                      options={options}
+                                      onBlur={formikProps.handleBlur}
+                                      menuPortalTarget={document.body}
+                                      isSearchable
+                                      isMulti
+                                      placeholder={"Select Tags"}
+                                    />
+                                    <ErrorMessage
+                                      name="tags"
+                                      component="div"
+                                      className="text-danger"
+                                    />
+                                  </FormGroup>
+                                </Col>
+
+                                <Col lg="6">
+                                  <FormGroup>
+                                    <label
+                                      className="form-control-label"
+                                      htmlFor="input-title"
+                                    >
+                                      Category
+                                    </label>
+                                    <Select
+                                      type="text"
+                                      className="form-control-alternative"
+                                      name="category"
+                                      onChange={(option) => {
+                                        option
+                                          ? formikProps.setFieldValue(
+                                              "category",
+                                              option
+                                            )
+                                          : formikProps.setFieldValue(
+                                              "category",
+                                              ""
+                                            );
+                                      }}
+                                      options={options}
+                                      onBlur={formikProps.handleBlur}
+                                      menuPortalTarget={document.body}
+                                      isSearchable
+                                      isClearable
+                                      placeholder={"Select Category"}
+                                    />
+                                    <ErrorMessage
+                                      name="category"
+                                      component="div"
+                                      className="text-danger"
+                                    />
+                                  </FormGroup>
+                                </Col>
+                              </Row>
+                            </div>
+
+                            <hr className="my-4" />
+                            <h6 className="heading-small text-muted mb-4">
+                              Blog Details
+                            </h6>
+                            <div className="pl-lg-4">
+                              <FormGroup>
+                                <label>Blog Content</label>
+                                <Field
+                                  className="form-control form-control-alternative"
+                                  placeholder="A few words about you ..."
+                                  rows="4"
+                                  type="textarea"
+                                  name="content"
+                                />
+                                <ErrorMessage
+                                  name="content"
+                                  component="div"
+                                  className="text-danger"
+                                />
+                              </FormGroup>
+                              <Button type="submit" className="btn btn-primary">
+                                Submit
+                              </Button>
+                            </div>
+                          </Form>
+                        )}
+                      </Formik>
+                    </CardBody>
+                  </Card>
+                </Col>
+                {/* <div className="container mt-5">
+                  <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                      <h2>Registration Form</h2>
+                      <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={handleSubmit}
+                      >
+                        <Form>
+                          <div className="form-group">
+                            <label htmlFor="title">Username</label>
+                            <Field
+                              type="text"
+                              id="title"
+                              name="title"
+                              className="form-control"
+                            />
+                            <ErrorMessage
+                              name="title"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </div>
+
+                          <div className="form-group">
+                            <label htmlFor="email">Email Address</label>
+                            <Field
+                              type="text"
+                              id="email"
+                              name="email"
+                              className="form-control"
+                            />
+                            <ErrorMessage
+                              name="email"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </div>
+
+                          <div className="form-group">
+                            <label htmlFor="aboutMe">About Me</label>
+                            <Field
+                              as="textarea"
+                              id="aboutMe"
+                              name="aboutMe"
+                              className="form-control"
+                            />
+                            <ErrorMessage
+                              name="aboutMe"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </div>
+
+                          <div className="form-group">
+                            <label htmlFor="postalCode">Postal Code</label>
+                            <Field
+                              type="text"
+                              id="postalCode"
+                              name="postalCode"
+                              className="form-control"
+                            />
+                            <ErrorMessage
+                              name="postalCode"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </div>
+
+                          <button type="submit" className="btn btn-primary">
+                            Submit
+                          </button>
+                        </Form>
+                      </Formik>
+                    </div>
+                  </div>
+                </div> */}
               </CardBody>
             </Card>
           </div>
