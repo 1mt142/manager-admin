e921521005dd0cb263b2d0275f06ce37dc243071
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2023-09-18 17:18:43 +0600

    Blog Filter

--- src/views/pages/Blog/Blogs.js
+++ src/views/pages/Blog/Blogs.js
@@ -10,15 +10,23 @@ import {
   Container,
   Row,
   Button,
+  Col,
+  FormGroup,
 } from "reactstrap";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Select from "react-select";
 import Header from "components/Headers/Header.js";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { getPublicData } from "core/apiClient";
 import { GET_BLOGS_API } from "core/apiEndpoints";
 
 const Blogs = () => {
   const [data, setData] = useState({});
+  const [dropdownKey, setDropdownKey] = useState(0);
+  // const { id } = useParams();
+  const formRef = useRef(null);
   async function getPosts(params = {}) {
     await getPublicData(GET_BLOGS_API, params)
       .then((response) => {
@@ -26,6 +34,26 @@ const Blogs = () => {
       })
       .catch((err) => console.log(err));
   }
+
+  const initialValues = {
+    // username: data.username || "",
+    // email: data.email || "",
+    // password: "",
+    // user_type: data.user_type || "",
+  };
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("This field is required"),
+    email: Yup.string().required("This field is required"),
+    user_type: Yup.string().required("This field is required"),
+    password: Yup.string().required("This field is required"),
+  });
+
+  const handleSubmitMethod = (values, { resetForm }) => {
+    // postBlog(values);
+    resetForm();
+    setDropdownKey(dropdownKey + 1);
+    formRef.current.reset();
+  };
 
   useEffect(() => {
     getPosts({
@@ -39,12 +67,138 @@ const Blogs = () => {
       <Header />
       {/* Page content */}
       <Container className="mt--7" fluid>
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0 d-flex align-items-center justify-content-between">
+                <h3 className="mb-0">Filter Blogs</h3>
+              </CardHeader>
+              <Formik
+                enableReinitialize
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmitMethod}
+              >
+                {(formikProps) => (
+                  <Form onSubmit={formikProps.handleSubmit} ref={formRef}>
+                    <div className="pl-lg-4">
+                      <Row>
+                        <Col md="3">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-title"
+                            >
+                              Title
+                            </label>
+                            <Field
+                              className="form-control form-control-alternative"
+                              id="input-title"
+                              name="title"
+                              placeholder="Name"
+                              type="text"
+                            />
+                            <ErrorMessage
+                              name="title"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </FormGroup>
+                        </Col>
+
+                        <Col>
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-tag"
+                            >
+                              Tag
+                            </label>
+                            <Select
+                              key={dropdownKey}
+                              type="text"
+                              className="form-control-alternative"
+                              name="tag"
+                              onChange={(selectedOption) => {
+                                formikProps.setFieldValue(
+                                  "tag",
+                                  selectedOption?.value
+                                );
+                              }}
+                              options={[
+                                { label: "GOST", value: "gost" },
+                                { label: "Admin", value: "Admin" },
+                              ]}
+                              onBlur={formikProps.handleBlur}
+                              menuPortalTarget={document.body}
+                              isSearchable
+                              isClearable
+                            />
+                            <ErrorMessage
+                              name="tag"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </FormGroup>
+                        </Col>
+
+                        <Col>
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-category"
+                            >
+                              Category
+                            </label>
+                            <Select
+                              key={dropdownKey}
+                              type="text"
+                              className="form-control-alternative"
+                              name="category"
+                              onChange={(selectedOption) => {
+                                formikProps.setFieldValue(
+                                  "category",
+                                  selectedOption?.value
+                                );
+                              }}
+                              options={[
+                                { label: "GOST", value: "gost" },
+                                { label: "Admin", value: "Admin" },
+                              ]}
+                              onBlur={formikProps.handleBlur}
+                              menuPortalTarget={document.body}
+                              isSearchable
+                              isClearable
+                            />
+                            <ErrorMessage
+                              name="category"
+                              component="div"
+                              className="text-danger"
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col md="3">
+                          <div className="mt-4">
+                            <Button type="submit" className="btn btn-success">
+                              Search
+                            </Button>
+                          </div>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </Card>
+          </div>
+        </Row>
+
         {/* Table */}
         <Row>
           <div className="col">
             <Card className="shadow">
               <CardHeader className="border-0 d-flex align-items-center justify-content-between">
-                <h3 className="mb-0">All Blogs</h3>
+                <h3 className="mb-0">ALL BLOGS</h3>
                 <Link to="/admin/blogs/create">
                   <Button color="primary">Create</Button>
                 </Link>
