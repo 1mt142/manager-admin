a9dea2422cbef692aed3051a19dd9a790e8ff9d0
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2023-09-19 13:39:08 +0600

    Details formated

--- src/views/pages/Blog/BlogDetails.js
+++ src/views/pages/Blog/BlogDetails.js
@@ -1,4 +1,12 @@
-import { Card, CardHeader, Container, Row, CardBody, Button } from "reactstrap";
+import {
+  Card,
+  CardHeader,
+  Container,
+  Row,
+  CardBody,
+  Button,
+  CardTitle,
+} from "reactstrap";
 import Header from "components/Headers/Header.js";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -34,28 +42,32 @@ const BlogDetails = () => {
               <CardHeader className="border-0">
                 <h3 className="mb-0">Blog Details</h3>
               </CardHeader>
+              <CardTitle className="text-center ">
+                <h1>{data.title}</h1>
+              </CardTitle>
               <CardBody>
-                <h1>
-                  <b>{data.title}</b>
-                </h1>
-                <div>
+                <div className="mt-1 mb-1 text-center">
                   <span
-                    style={{ width: "fitContent" }}
+                    style={{ width: "fitContent", fontSize: "10px" }}
                     className="bg-primary text-white p-2 m-1 rounded"
                   >
                     {data ? data?.category?.name : ""}
                   </span>
                   {data?.tags?.map((item) => (
                     <span
-                      style={{ width: "fitContent" }}
+                      style={{ width: "fitContent", fontSize: "10px" }}
                       className="bg-green text-white p-2 m-1 rounded"
                     >
                       #{item.title}
                     </span>
                   ))}
                 </div>
-                <div className="pt-2">
-                  <img src={`${BASE_API}/${data.imagePath}`} alt="Blog" />
+                <div className="pt-2 text-center">
+                  <img
+                    width="50%"
+                    src={`${BASE_API}/${data.imagePath}`}
+                    alt="Blog"
+                  />
                 </div>
 
                 <div dangerouslySetInnerHTML={{ __html: data.content }}></div>
